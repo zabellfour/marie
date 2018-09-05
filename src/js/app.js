342399aeef946693f17d52070f6e0a06012f4f64
zabellfour <zabell2814@gmail.com>
2018-09-06 01:02:48 +0300

    added sceleton of project

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+// You can write a call and import your functions in this file.
+//
+// This file will be compiled into app.js and will not be minified.
+// Feel free with using ES6 here.
+
+import common from './modules/common';
+
+( ($) => {
+  'use strict';
+
+  // When DOM is ready
+  $(() => {
+    common.mobileManu();
+  });
+
+})(jQuery);
---
+++ src/js/modules/common.js
@@ -0,0 +1,12 @@
+// This is an examples of simple export.
+//
+// You can remove or add your own function in this file.
+
+var common = {
+    mobileManu: function() {
+        console.log('here will be mobileManu function')
+    }
+};
+
+
+export default common;
