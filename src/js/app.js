af31247b863a8e4a0bbf55a42c7e2ddddd4f0de3
zabellfour <zabell2814@gmail.com>
2018-09-27 00:51:49 +0300

    vw start

--- src/js/app.js
+++ src/js/app.js
@@ -56,35 +56,35 @@ import { wrap } from 'module';
         });
 
 
-        $(window).resize(function() {
-            var mobileDelta = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase())) ? 0 : 17;
-            var w = $(window).width() + mobileDelta;
-            var h = $(window).height();
-            var scale;
-            var hw = $('.wrapper').height();
-            if (w >= 768 && w < 1920) {
-                var wspec = 1920;
-                scale = w / wspec;
-                // console.log(hw);
-                //           $('body').height(hw * scale);
-                isIEorEDGE() ? $(document).scroll(function(e) {
-                    e.preventDefault()
-                    var hw = ($('.footer')[0].offsetTop - $('.footer').height() * 1.65) * scale;
-                    var maxScroll = hw;
-                    if ($(document).scrollTop() > maxScroll) {
-                        $(document).scrollTop(maxScroll)
-                    }
-                    console.log($(document).scrollTop(), hw);
-                }) : '';
-            } else {
-                scale = 1;
-                mobileDelta = 0;
-            }
+        // $(window).resize(function() {
+        //     var mobileDelta = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase())) ? 0 : 17;
+        //     var w = $(window).width() + mobileDelta;
+        //     var h = $(window).height();
+        //     var scale;
+        //     var hw = $('.wrapper').height();
+        //     if (w >= 768 && w < 1920) {
+        //         var wspec = 1920;
+        //         scale = w / wspec;
+        //         // console.log(hw);
+        //         //           $('body').height(hw * scale);
+        //         isIEorEDGE() ? $(document).scroll(function(e) {
+        //             e.preventDefault()
+        //             var hw = ($('.footer')[0].offsetTop - $('.footer').height() * 1.65) * scale;
+        //             var maxScroll = hw;
+        //             if ($(document).scrollTop() > maxScroll) {
+        //                 $(document).scrollTop(maxScroll)
+        //             }
+        //             console.log($(document).scrollTop(), hw);
+        //         }) : '';
+        //     } else {
+        //         scale = 1;
+        //         mobileDelta = 0;
+        //     }
 
-            $(".wrapper").css('transform', 'scale(' + scale + ')').css('margin', -mobileDelta / 2);
+        //     $(".wrapper").css('transform', 'scale(' + scale + ')').css('margin', -mobileDelta / 2);
 
 
-        });
+        // });
 
     });
 
